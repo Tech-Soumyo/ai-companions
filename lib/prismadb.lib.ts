42a1160de8df447162669dda701abe66ef9b1b91
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-09 18:06:49 +0530

    redesign the prismadb file

--- lib/prismadb.lib.ts
+++ lib/prismadb.lib.ts
@@ -1,8 +1,13 @@
-import { Prisma } from "@prisma/client";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
+  // Allow global `var` declarations
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
 const prismadb = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
