d405fb11166091486ce245571f4140cf5eca252e
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-07-09 18:07:19 +0530

    search filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,16 @@
+import { Categories } from "@/components/Categories";
 import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb.lib";
 import { UserButton } from "@clerk/nextjs";
+import { Category } from "@prisma/client";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
   return (
     <>
       <div className=" h-full p-4 space-y-2">
         <SearchInput />
+        <Categories data={categories} />
       </div>
     </>
   );
